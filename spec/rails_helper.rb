4a4bad3f15d230a3be68b2001daf9c39ee9f373d
song55555 <hil55555@naver.com>
2025-01-10 17:46:40 +0900

    バリデーションエラーメッセージのデザイン調整

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,5 +20,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::IntegrationHelpers, type: :system
 
 end
